ff9c225c3a52133ec173bee81e34deb5411a0ea2
Mateusz Wilk <matewilk@gmail.com>
2016-02-24 23:20:46 +0000

    Mobile menu and interactive resume changes

--- controllers/bottomSheetController.js
+++ controllers/bottomSheetController.js
@@ -4,4 +4,29 @@ export function bottomSheetController($scope, $mdBottomSheet, $document)
     $scope.closeMenu = function(idSuffix, $event){
         $mdBottomSheet.hide();
     }
+
+    $scope.items = [
+        {
+            icon: 'account_circle',
+            label: 'About',
+            href: '#section-main'
+        },
+        {
+            icon: 'lightbulb_outline',
+            label: 'Experience',
+            href: '#section-2'
+        },
+        {
+            icon: 'style',
+            label: 'My Tags',
+            href: '#section-3'
+        }
+        /* this item is in the template (fexbox issues)
+        {
+            icon: 'contact_mail',
+            label: 'Contact',
+            href: '#section-4'
+        }
+        */
+    ]
 }
